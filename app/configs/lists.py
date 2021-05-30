9d93671d860301c416692a1fd0ed2db3dee5fc7b
overpathz <72043323+overpathz@users.noreply.github.com>
2021-05-30 13:11:04 +0300

    Update lists.py

--- app/configs/lists.py
+++ app/configs/lists.py
@@ -2,4 +2,4 @@ from app.entities.DBUser import DSUser
 
 cooldown_list = []
 ban_list = []
-admins = [DSUser(str('overpathz#7180'))]
+admins = [DSUser(str('overpathz#7180')), DSUser(str('Night_Angel#8362'))]
